21fe577d54d297e31688b42c8ff0132c686841cb
jahrim <jahrim.cesario2@studio.unibo.it>
2023-06-03 12:14:44 +0200

    build: add execution and test of the application

---
+++ src/test/scala/test/TemplateTest.scala
@@ -0,0 +1,13 @@
+package test
+
+/** Template test. */
+class TemplateTest extends AbstractTest:
+  private val positiveValue = 1
+
+  describe("A value") {
+    describe("when positive") {
+      it ("should be greater than 0") {
+        positiveValue should be > 0
+      }
+    }
+  }
